bfd55c77f5ecf41183f3b8704a5807d328a38720
Neptade <loic@lozolozo.com>
2023-05-18 17:56:19 +0200

    fixed bug where path not found

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 const express = require('express');
+const path = require('path');
 const dotenv = require('dotenv');
 dotenv.config({path: '.env'});
 const port = process.env.port;
 const app = express();
-const mariadb = require('mariadb')
+const mariadb = require('mariadb');
+
+
 
 
 
 app.get('/', (request, response) => { 
-    const filePath = path.join(env.rootDirectory)
-    response.sendFile("home.html")
+    const filePath = path.join(process.env.rootDirectory, "home.html")
+    response.sendFile(filePath)
 });
 
 app.listen(port, () => {
     console.log(`Listening for requests on port ${port}`)
-})
+})  
+
+
+
+app.use("/home", require("./controllers/home.route"));
